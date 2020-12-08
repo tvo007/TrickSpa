086abdbfdc52fff43c3d65f124695eb429773b15
Tim Vo <timtvo714@gmail.com>
2020-12-07 18:39:35 -0800

    tvo's new main branch

--- src/views/Forums/index.js
+++ src/views/Forums/index.js
@@ -1,48 +1,58 @@
 import React, {useEffect} from 'react';
-import PropTypes from 'prop-types'
-import {getPosts} from '../../actions/post'
+import PropTypes from 'prop-types';
+import {getPosts} from '../../actions/post';
+import {getComments} from '../../actions/comment';
 import {useDispatch, useSelector} from 'react-redux';
-import {Grid, Typography} from '@material-ui/core'
+import {Grid, Typography} from '@material-ui/core';
 
-const Forums = (props) => {
+const Forums = props => {
+  const dispatch = useDispatch ();
 
-    const dispatch = useDispatch()
+  useEffect (
+    () => {
+      dispatch (getPosts ());
+      dispatch (getComments (39));
+    },
+    [dispatch]
+  );
 
-    useEffect (
-        () => {
-          dispatch (getPosts ());
-        },
-        [dispatch]
-      );
 
-      
   const {posts, isPostLoading} = useSelector (state => state.post);
+  const {comments, isCommentLoading} = useSelector (state => state.comment);
 
-    return (
-        <Grid>
-            <Grid>
-            <Typography>
-                This is the forums page.
-            </Typography>
-            </Grid>
-            <Grid>
-            <Typography>
-                # of posts: {posts.length}
-            </Typography>
-            <Typography>
-                Todo: expand into a fully functional message board
-            </Typography>
+  const showComments = comments.map( comment => {
+    return <Grid>
+      <Typography>{comment.body}</Typography>
+    </Grid>
+  })
 
-            </Grid>
-            
-        </Grid>
-         
-    )
-}
+  return (
+    <Grid>
+      <Grid>
+        <Typography>
+          This is the forums page.
+        </Typography>
+      </Grid>
+      <Grid>
+        <Typography>
+          # of posts: {posts.length}
+        </Typography>
+        <Typography>
+            # of comments {comments.length}
+        </Typography>
+        <Typography>
+            Actual Comments: {showComments}
+        </Typography>
+        <Typography>
+          Todo: expand into a fully functional message board
+        </Typography>
 
-Forums.propTypes = {
+      </Grid>
 
-}
+    </Grid>
+  );
+};
 
-export default Forums
+Forums.propTypes = {};
 
+export default Forums;
