6a9adecc3e9c185eb0a70dddfb0ef535a928abc5
Tim Vo <timtvo714@gmail.com>
2021-01-17 20:20:20 -0800

    added prelim comments compoents

--- src/views/Post/PostView.js
+++ src/views/Post/PostView.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import PageHeading from '../../components/PageHeading';
 import OriginalPost from './components/OriginalPost';
+import Comment from './components/Comment';
 import {
   Grid,
   Typography,
@@ -12,9 +13,18 @@ import {
 } from '@material-ui/core';
 
 const PostView = ({post, isPostLoading, error}) => {
+  const author = post.user ? post.user.username : null;
 
-  const author = post.user ? post.user.username : null
-
+  const comments = post.comments
+    ? post.comments.map (comment => (
+        <Comment
+          key={comment.id}
+          body={comment.body}
+          title={post.title}
+          author={comment.user.username}
+        />
+      ))
+    : null;
 
   return (
     <Grid container spacing={4}>
@@ -29,20 +39,23 @@ const PostView = ({post, isPostLoading, error}) => {
 
                 <Grid item xs={12}>
                   {/**this is where we can start creating a specific component to decorate the OP  */}
-                  <OriginalPost title={post.title} body={post.body} author={author} post={post}/>
+                  <OriginalPost
+                    title={post.title}
+                    body={post.body}
+                    author={author}
+                    post={post}
+                  />
 
                 </Grid>
                 <Grid item xs={12}>
-                  <Card>
-                    <CardHeader
-                      title={
-                        <Typography variant="h4">COMMENT TITLE HERE</Typography>
-                      }
-                    />
-                    <CardContent>
-                      <Typography>COMMENT BODY HERE</Typography>
-                    </CardContent>
-                  </Card>
+                  {/**to be mapped */}
+
+                  {/* {post.comments
+                    ? post.comments.map (comment => (
+                        <Comment key={comment.id} body={comment.body} title={post.title}/>
+                      ))
+                    : null} */}
+                  {comments}
                 </Grid>
               </Fragment>}
 
