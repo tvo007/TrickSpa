3b93e5f0f718733e5a525795c19caba936193f1c
Beau Sherman <brsherman88@gmail.com>
2021-01-21 19:23:20 -0800

    Move useForm & validateFormInfo to common

--- src/common/useForm.js
+++ src/common/useForm.js
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 import validateFormInfo from './validateFormInfo';
 
 function useForm(formValues) {
--- src/views/Landing/components/SigninForm.js
+++ src/views/Landing/components/SigninForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import useForm from '../useForm';
+import useForm from '../../../common/useForm';
 import { Paper, Typography, TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
