f54902568e3096e39ab407397ed7a935f5b55df5
Beau Sherman <brsherman88@gmail.com>
2021-03-07 20:15:32 -0800

    Add more cards to DummyProfileView and adjust Main styles

--- src/views/Profile/DummyProfileView.js
+++ src/views/Profile/DummyProfileView.js
@@ -1,5 +1,3 @@
-//to be deleted
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
@@ -8,22 +6,33 @@ import {
   Typography,
   Card,
   CardHeader,
-  CardContent
+  CardContent,
+  Divider
 } from '@material-ui/core';
 import PageHeading from '../../components/PageHeading';
 import { makeStyles } from '@material-ui/core/styles';
+
 const useStyles = makeStyles(theme => ({
   root: {
+    marginTop: '-3rem'
+  },
+  mainCard: {
+    paddingTop: '2rem',
     textAlign: 'center'
+  },
+  card: {
+    marginBottom: '1rem'
   },
   divider: {
     background: 'black'
   },
-  card: {
-    paddingTop: '2rem'
-  },
   subheader: {
     marginTop: '1rem'
+  },
+  list: {
+    '& > *': {
+      margin: '1rem 0'
+    }
   },
   avatar: {
     position: 'relative',
@@ -42,10 +51,10 @@ const useStyles = makeStyles(theme => ({
 const DummyProfileView = props => {
   const classes = useStyles();
   return (
-    <Grid className={classes.root} container>
+    <Grid className={classes.root} container item spacing={2} xs={12}>
       <Grid item xs={12}>
         <Avatar className={classes.avatar}>JD</Avatar>
-        <Card className={classes.card}>
+        <Card className={classes.mainCard}>
           <CardHeader
             subheader={
               <Typography className={classes.subheader}>
@@ -62,6 +71,81 @@ const DummyProfileView = props => {
             </Typography>
             <Typography variant="body1">
               Maybe this user's posts or their favorite posts by other people?
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item sm={4}>
+        <Card className={classes.card}>
+          <CardHeader
+            title={<Typography variant="h4">Organizations</Typography>}
+          />
+          <Divider />
+          <CardContent className={classes.list}>
+            <Typography>Some organization</Typography>
+            <Typography>Some organization</Typography>
+            <Typography>Some organization</Typography>
+            <Typography>Some organization</Typography>
+            <Typography>Some organization</Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader
+            title={<Typography variant="h4">Other stuff</Typography>}
+          />
+          <Divider />
+          <CardContent className={classes.list}>
+            <Typography>Some other thing</Typography>
+            <Typography>Some other thing</Typography>
+            <Typography>Some other thing</Typography>
+            <Typography>Some other thing</Typography>
+            <Typography>Some other thing</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid container item sm={8}>
+        <Card className={classes.card}>
+          <CardHeader title={<Typography variant="h4">Some post</Typography>} />
+          <CardContent>
+            <Typography>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+              velit odit sunt provident maiores, perferendis aliquam minus fugit
+              similique tempora animi consequatur ducimus repellendus architecto
+              molestias corporis officia consectetur! Odit! Nemo dolore
+              architecto eum eos fugiat ea aperiam impedit incidunt quae, harum
+              officiis praesentium neque exercitationem, earum quibusdam, iusto
+              adipisci eaque! Earum fugit nostrum officiis? Sequi doloribus enim
+              placeat velit.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card className={classes.card}>
+          <CardHeader title={<Typography variant="h4">Some post</Typography>} />
+          <CardContent>
+            <Typography>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+              velit odit sunt provident maiores, perferendis aliquam minus fugit
+              similique tempora animi consequatur ducimus repellendus architecto
+              molestias corporis officia consectetur! Odit! Nemo dolore
+              architecto eum eos fugiat ea aperiam impedit incidunt quae, harum
+              officiis praesentium neque exercitationem, earum quibusdam, iusto
+              adipisci eaque! Earum fugit nostrum officiis? Sequi doloribus enim
+              placeat velit.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card className={classes.card}>
+          <CardHeader title={<Typography variant="h4">Some post</Typography>} />
+          <CardContent>
+            <Typography>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+              velit odit sunt provident maiores, perferendis aliquam minus fugit
+              similique tempora animi consequatur ducimus repellendus architecto
+              molestias corporis officia consectetur! Odit! Nemo dolore
+              architecto eum eos fugiat ea aperiam impedit incidunt quae, harum
+              officiis praesentium neque exercitationem, earum quibusdam, iusto
+              adipisci eaque! Earum fugit nostrum officiis? Sequi doloribus enim
+              placeat velit.
             </Typography>
           </CardContent>
         </Card>
--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -1,15 +1,16 @@
 //to be deleted
 
 import React from 'react';
+import { Grid } from '@material-ui/core';
 // import ProfileView from './ProfileView'
 import DummyProfileView from './DummyProfileView';
 // import PropTypes from 'prop-types';
 
 const Profile = props => {
   return (
-    <div>
+    <>
       <DummyProfileView />
-    </div>
+    </>
   );
 };
 
