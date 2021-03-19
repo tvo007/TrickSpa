05d01f88c1b3751fd75ec0e6643525d81615fce2
Beau Sherman <brsherman88@gmail.com>
2021-03-18 19:22:20 -0700

    Add basic layout to SettingsView

--- src/views/Settings/SettingsView.js
+++ src/views/Settings/SettingsView.js
@@ -3,20 +3,53 @@ import React from 'react';
 import {
   Grid,
   Typography,
-//   Card,
-//   CardHeader,
-//   CardContent,
-//   Divider,
+  Card,
+  CardHeader,
+  CardContent,
+  Divider
 } from '@material-ui/core';
 import PageHeading from '../../components/PageHeading';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  card: {
+    marginBottom: theme.spacing(2)
+  },
+  user: {},
+  basic: {}
+}));
 
 const SettingsView = props => {
+  const classes = useStyles();
+
+  const username = 'Jon Snow';
+
   return (
-    <Grid container spacing={4}>
+    <Grid container spacing={4} xs={12}>
       <Grid item xs={12}>
-        <PageHeading title="Settings" />
+        <PageHeading title={`Settings for ${username}`} />
       </Grid>
-      <Typography>This is the settings page</Typography>
+      <Grid item xs={4}>
+        <Card>
+          <Typography>Profile</Typography>
+        </Card>
+        <Typography>Account</Typography>
+      </Grid>
+      <Grid item xs={8}>
+        <Card className={classes.card}>
+          <CardHeader title="User" />
+          <CardContent>
+            <Typography>Some Fields go here</Typography>
+          </CardContent>
+        </Card>
+        <Card className={classes.card}>
+          <CardHeader title="Basic" />
+          <CardContent>
+            <Typography>Some Fields go here</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
     </Grid>
   );
 };
